7654423dc2d3b3cfd591bb81d9b7aa8deedf625c
Quin Derbi <quinderbi@gmail.com>
2022-11-19 00:09:23 +0700

    add schedule

--- src/Schedule/Schedule.java
+++ src/Schedule/Schedule.java
@@ -1,5 +1,6 @@
 package Schedule;
 
+import java.time.LocalDateTime;
 import Studio.Studio;
 import Movie.Movie;
 import Ticket.Ticket;
@@ -9,16 +10,61 @@ public class Schedule {
     private String scheduleID;
     private Movie movie;
     private Studio studio;
-    private String date;
-    private String time;
+    private LocalDateTime dateTime;
     private Ticket[] tickets;
 
     public Schedule(String scheduleID, Movie movie, Studio studio, String date, String time) {
         this.scheduleID = scheduleID;
         this.movie = movie;
         this.studio = studio;
-        this.date = date;
-        this.time = time;
+        this.dateTime = LocalDateTime.parse(date + "T" + time);
+        this.tickets = new Ticket[studio.getSeats().length];
+    }
+
+    
+    public String getScheduleID() {
+        return scheduleID;
+    }
+    
+    public void setScheduleID(String scheduleID) {
+        this.scheduleID = scheduleID;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Studio getStudio() {
+        return studio;
+    }
+
+    public void setStudio(Studio studio) {
+        this.studio = studio;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+    
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Ticket[] getTickets() {
+        return tickets;
+    }
+
+    public void addTicket(Ticket ticket) {
+        for (int i = 0; i < tickets.length; i++) {
+            if (tickets[i] == null) {
+                tickets[i] = ticket;
+                break;
+            }
+        }
     }
 
     // get Available Seats
